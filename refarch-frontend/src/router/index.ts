b7ffa51c4de74fa6efae55e8e3627c946bac6f07
Der-Alex-K <18263366+Der-Alex-K@users.noreply.github.com>
2024-07-26 11:39:49 +0200

    switch to createWebHashHistory

--- refarch-frontend/src/router/index.ts
+++ refarch-frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 // Composables
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import { ROUTES_GETSTARTED, ROUTES_HOME } from "@/Constants";
 import GetStartedView from "@/views/GetStartedView.vue";
@@ -21,7 +21,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
