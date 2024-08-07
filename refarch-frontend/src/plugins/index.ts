20b98704fa560cc9b896855b3577076d83a0602f
devtobi <28538704+devtobi@users.noreply.github.com>
2024-08-07 12:35:57 +0200

    :recycle: Moved router to plugins

--- refarch-frontend/src/plugins/index.ts
+++ refarch-frontend/src/plugins/index.ts
@@ -1,8 +1,8 @@
 import type { App } from "vue";
 
 import pinia from "@/plugins/pinia";
+import router from "@/plugins/router";
 import vuetify from "@/plugins/vuetify";
-import router from "@/router";
 
 export function registerPlugins(app: App) {
   app.use(vuetify).use(router).use(pinia);
