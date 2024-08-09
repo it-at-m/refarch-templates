e9261a762f9301cece9ef261815ea8c8a0a3f77c
devtobi <28538704+devtobi@users.noreply.github.com>
2024-08-09 11:20:49 +0200

    :recycle: Fix PMD errors

--- refarch-backend/src/main/java/de/muenchen/refarch/configuration/nfcconverter/NfcServletInputStream.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/configuration/nfcconverter/NfcServletInputStream.java
@@ -3,7 +3,6 @@ package de.muenchen.refarch.configuration.nfcconverter;
 import jakarta.servlet.ReadListener;
 import jakarta.servlet.ServletInputStream;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import org.apache.commons.lang3.NotImplementedException;
 
 /**
