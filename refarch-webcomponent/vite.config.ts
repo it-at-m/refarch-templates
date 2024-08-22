91f5347a68e9e828af774c808bf1e137536b26c4
devtobi <28538704+devtobi@users.noreply.github.com>
2024-08-22 10:28:00 +0200

    :wrench: Simplified vite config to work with API gateway of refarch stack (port 8083)

--- refarch-webcomponent/vite.config.ts
+++ refarch-webcomponent/vite.config.ts
@@ -2,19 +2,9 @@ import { fileURLToPath, URL } from "node:url";
 
 import { viteVueCESubStyle } from "@unplugin-vue-ce/sub-style";
 import vue from "@vitejs/plugin-vue";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import type { PluginOption } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-
-const portFromDevelopmentEnv = loadEnv("development", "./")?.VITE_SERVER_PORT;
-const port = portFromDevelopmentEnv
-  ? Number.parseInt(portFromDevelopmentEnv)
-  : 8081;
-
-const apiPortFromDevelopmentEnv = loadEnv("development", "./")?.VITE_API_PORT;
-const apiPort = apiPortFromDevelopmentEnv
-  ? Number.parseInt(apiPortFromDevelopmentEnv)
-  : 8083;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,11 +18,11 @@ export default defineConfig({
     cssInjectedByJsPlugin(),
   ],
   server: {
-    port,
+    port: 8082,
     proxy: {
-      "/api": `http://localhost:${apiPort}/`,
-      "/actuator": `http://localhost:${apiPort}/`,
-      "/clients": `http://localhost:${apiPort}/`,
+      "/api": "http://localhost:8083",
+      "/actuator": "http://localhost:8083",
+      "/clients": "http://localhost:8083",
     },
   },
   resolve: {
