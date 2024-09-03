969dfa334560fec7b0a68ae5fefa603a508832dc
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-09-03 15:26:50 +0200

    added springbootapplication annotation and removed unnecessary

--- refarch-backend/src/main/java/de/muenchen/refarch/MicroServiceApplication.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/MicroServiceApplication.java
@@ -1,25 +1,15 @@
 package de.muenchen.refarch;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Application class for starting the microservice.
  */
-@Configuration
-@ComponentScan(
-        basePackages = {
-                "org.springframework.data.jpa.convert.threeten",
-                "de.muenchen.refarch"
-        }
-)
 @EntityScan(
         basePackages = {
-                "org.springframework.data.jpa.convert.threeten",
                 "de.muenchen.refarch"
         }
 )
@@ -28,10 +18,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
                 "de.muenchen.refarch"
         }
 )
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = "de.muenchen.refarch")
 @SuppressWarnings("PMD.UseUtilityClass")
 public class MicroServiceApplication {
     public static void main(final String[] args) {
         SpringApplication.run(MicroServiceApplication.class, args);
     }
+
+
 }
