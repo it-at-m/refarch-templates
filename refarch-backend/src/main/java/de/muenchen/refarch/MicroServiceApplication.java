0e2e1439c11632411e09742f146f2de5aff714ee
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-09-03 16:40:35 +0200

    removed entityScan

--- refarch-backend/src/main/java/de/muenchen/refarch/MicroServiceApplication.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/MicroServiceApplication.java
@@ -2,17 +2,11 @@ package de.muenchen.refarch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Application class for starting the microservice.
  */
-@EntityScan(
-        basePackages = {
-                "de.muenchen.refarch"
-        }
-)
 @EnableJpaRepositories(
         basePackages = {
                 "de.muenchen.refarch"
