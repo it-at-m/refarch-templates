d0e3920b9517b6f2525aa5e45a86a9c4c93ef6da
eidottermihi <eidottermihi@gmail.com>
2024-09-04 10:58:36 +0200

    apply spotless on eai

--- refarch-eai/src/test/java/de/muenchen/refarch/TestConstants.java
+++ refarch-eai/src/test/java/de/muenchen/refarch/TestConstants.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @SuppressWarnings("PMD.TestClassWithoutTestCases")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class TestConstants {
-    
+
     public static final String SPRING_TEST_PROFILE = "test";
 
     public static final String SPRING_JSON_LOGGING_PROFILE = "json-logging";
