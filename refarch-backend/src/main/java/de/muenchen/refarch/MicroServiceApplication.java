559b704f30742deebd8286979774245c63946042
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-09-05 09:41:17 +0200

    Maint/182 maintenance refactor main entrypoint class (#280)
    * added springbootapplication annotation and removed unnecessary
    
    * removed empty lines
    
    * removed entityScan
    
    * removed entityScan

--- refarch-backend/src/main/java/de/muenchen/refarch/MicroServiceApplication.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/MicroServiceApplication.java
@@ -1,34 +1,12 @@
 package de.muenchen.refarch;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Application class for starting the microservice.
  */
-@Configuration
-@ComponentScan(
-        basePackages = {
-                "org.springframework.data.jpa.convert.threeten",
-                "de.muenchen.refarch"
-        }
-)
-@EntityScan(
-        basePackages = {
-                "org.springframework.data.jpa.convert.threeten",
-                "de.muenchen.refarch"
-        }
-)
-@EnableJpaRepositories(
-        basePackages = {
-                "de.muenchen.refarch"
-        }
-)
-@EnableAutoConfiguration
+@SpringBootApplication
 @SuppressWarnings("PMD.UseUtilityClass")
 public class MicroServiceApplication {
     public static void main(final String[] args) {
