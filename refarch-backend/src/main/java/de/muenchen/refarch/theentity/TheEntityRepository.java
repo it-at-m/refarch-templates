66d49923c51735c1ce66882d2a47f93919d82bb8
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-10-21 13:44:05 +0200

    removed comment and no sonar

--- refarch-backend/src/main/java/de/muenchen/refarch/theentity/TheEntityRepository.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/theentity/TheEntityRepository.java
@@ -5,16 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Provides a Repository for {@link TheEntity}. This Repository is exported as a REST resource.
- * <p>
- * The Repository handles CRUD Operations. Every Operation is secured and takes care of the tenancy.
- * For specific Documentation on how the generated REST point behaves, please consider the Spring
- * Data Rest Reference
- * <a href="http://docs.spring.io/spring-data/rest/docs/current/reference/html/">here</a>.
- * </p>
- */
+
 @Repository
-public interface TheEntityRepository extends PagingAndSortingRepository<TheEntity, UUID>, CrudRepository<TheEntity, UUID> { //NOSONAR
+public interface TheEntityRepository extends PagingAndSortingRepository<TheEntity, UUID>, CrudRepository<TheEntity, UUID> {
 
 }
