dcbefe538b4cf5fa60eddaa92099eeee7e67e763
devtobi <28538704+devtobi@users.noreply.github.com>
2024-11-13 14:57:43 +0100

    :bug: Fix post-build script

--- refarch-webcomponent/processes/post-build.js
+++ refarch-webcomponent/processes/post-build.js
@@ -1,6 +1,6 @@
 import path from "node:path";
 import { generateLoaderJs } from './lib/fileGenerator.js';
-import manifest from '../dist/src/.vite/manifest.json' assert {type: 'json'};
+import manifest from '../dist/src/.vite/manifest.json' with {type: 'json'};
 
 /**
  * Why this?
