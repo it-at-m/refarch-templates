93e2d71fe74829221f28280ee68945ab0f52b6e5
DanielOber <145556033+DanielOber@users.noreply.github.com>
2024-11-19 14:32:30 +0100

    added validation

--- refarch-backend/src/main/java/de/muenchen/refarch/configuration/SecurityProperties.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/configuration/SecurityProperties.java
@@ -1,8 +1,11 @@
 package de.muenchen.refarch.configuration;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "security")
-public record SecurityProperties(String requestLogging, String userInfoUri) {
+@Validated
+public record SecurityProperties(@NotBlank String requestLogging, @NotBlank String userInfoUri) {
 
 }
