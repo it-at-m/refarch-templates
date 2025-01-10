42a2b31baa32289f5e0350337f1e8f4341cbabf9
Tobias Stadler <28538704+devtobi@users.noreply.github.com>
2025-01-10 09:50:07 +0100

    :bug: Fix inspection functionality not working for Vue devtools (#689)

--- refarch-frontend/vite.config.ts
+++ refarch-frontend/vite.config.ts
@@ -8,41 +8,44 @@ import vueDevTools from "vite-plugin-vue-devtools";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-      features: {
-        optionsAPI: false,
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === "development";
+  return {
+    plugins: [
+      vue({
+        template: { transformAssetUrls },
+        features: {
+          optionsAPI: isDevelopment,
+        },
+      }),
+      vuetify({
+        autoImport: false,
+      }),
+      UnpluginFonts({
+        fontsource: {
+          families: [
+            {
+              name: "Roboto",
+              weights: [100, 300, 400, 500, 700, 900],
+              subset: "latin",
+            },
+          ],
+        },
+      }),
+      vueDevTools(),
+    ],
+    server: {
+      host: true,
+      port: 8081,
+      proxy: {
+        "/api": "http://localhost:8083",
+        "/actuator": "http://localhost:8083",
       },
-    }),
-    vuetify({
-      autoImport: false,
-    }),
-    UnpluginFonts({
-      fontsource: {
-        families: [
-          {
-            name: "Roboto",
-            weights: [100, 300, 400, 500, 700, 900],
-            subset: "latin",
-          },
-        ],
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
-    }),
-    vueDevTools(),
-  ],
-  server: {
-    host: true,
-    port: 8081,
-    proxy: {
-      "/api": "http://localhost:8083",
-      "/actuator": "http://localhost:8083",
     },
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+  };
 });
