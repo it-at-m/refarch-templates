bb8b84e7153ad8af67d64b9bd0b6c14ef57af636
devtobi <28538704+devtobi@users.noreply.github.com>
2025-04-26 23:39:16 +0200

    :rotating_light: Fix unmapped property warning in mapper

--- refarch-backend/src/main/java/de/muenchen/refarch/theentity/dto/TheEntityMapper.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/theentity/dto/TheEntityMapper.java
@@ -2,11 +2,13 @@ package de.muenchen.refarch.theentity.dto;
 
 import de.muenchen.refarch.theentity.TheEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface TheEntityMapper {
 
     TheEntityResponseDTO toDTO(TheEntity theEntity);
 
+	@Mapping(target = "id", ignore = true)
     TheEntity toEntity(TheEntityRequestDTO theEntityRequestDTO);
 }
