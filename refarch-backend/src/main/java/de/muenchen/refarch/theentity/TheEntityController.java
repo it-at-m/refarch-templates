29f09f1b0cc333f4910c14bd4620f215b42bb4da
jannik.lange <jannik.lange@muenchen.de>
2025-05-21 13:11:35 +0200

    added some example descriptions and summaries

--- refarch-backend/src/main/java/de/muenchen/refarch/theentity/TheEntityController.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/theentity/TheEntityController.java
@@ -3,6 +3,8 @@ package de.muenchen.refarch.theentity;
 import de.muenchen.refarch.theentity.dto.TheEntityMapper;
 import de.muenchen.refarch.theentity.dto.TheEntityRequestDTO;
 import de.muenchen.refarch.theentity.dto.TheEntityResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
@@ -25,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "the_entity_example_tag", description = "An example tag for the entity controller")
 @RequestMapping("/theEntity")
 public class TheEntityController {
 
@@ -32,34 +35,38 @@ public class TheEntityController {
     private final TheEntityMapper theEntityMapper;
 
     @GetMapping("{theEntityID}")
+    @Operation(summary = "Retrieve an entity by its UUID", description = "Fetches the entity details using the provided UUID.")
     @ResponseStatus(HttpStatus.OK)
     public TheEntityResponseDTO getTheEntity(@PathVariable("theEntityID") final UUID theEntityId) {
         return theEntityMapper.toDTO(theEntityService.getTheEntity(theEntityId));
     }
 
     @GetMapping
+    @Operation(summary = "Retrieve entities with pagination", description = "Fetches a paginated list of entities based on the provided page number and size.")
     @ResponseStatus(HttpStatus.OK)
-    public Page<TheEntityResponseDTO> getTheEntitiesByPageAndSize(@RequestParam(defaultValue = "0") final int pageNumber,
-            @RequestParam(defaultValue = "10") final int pageSize) {
+    public Page<TheEntityResponseDTO> getTheEntitiesByPageAndSize(@RequestParam(defaultValue = "0") final int pageNumber,  @RequestParam(defaultValue = "10") final int pageSize) {
         final Page<TheEntity> pageWithEntity = theEntityService.getAllEntities(pageNumber, pageSize);
         final List<TheEntityResponseDTO> theEntityRequestDTOList = pageWithEntity.getContent().stream().map(theEntityMapper::toDTO).toList();
         return new PageImpl<>(theEntityRequestDTOList, pageWithEntity.getPageable(), pageWithEntity.getTotalElements());
     }
 
     @PostMapping
+    @Operation(summary = "Create a new entity", description = "Creates a new entity using the provided entity details.")
     @ResponseStatus(HttpStatus.CREATED)
     public TheEntityResponseDTO saveTheEntity(@Valid @RequestBody final TheEntityRequestDTO theEntityRequestDTO) {
         return theEntityMapper.toDTO(theEntityService.createTheEntity(theEntityMapper.toEntity(theEntityRequestDTO)));
     }
 
     @PutMapping("/{theEntityId}")
+    @Operation(summary = "Update an existing entity", description = "Updates the details of an existing entity using the provided UUID and entity details.")
     @ResponseStatus(HttpStatus.OK)
     public TheEntityResponseDTO updateTheEntity(@Valid @RequestBody final TheEntityRequestDTO theEntityRequestDTO,
-            @PathVariable("theEntityId") final UUID theEntityId) {
+                                                @PathVariable("theEntityId") final UUID theEntityId) {
         return theEntityMapper.toDTO(theEntityService.updateTheEntity(theEntityMapper.toEntity(theEntityRequestDTO), theEntityId));
     }
 
     @DeleteMapping("/{theEntityId}")
+    @Operation(summary = "Delete an entity", description = "Deletes the entity using the provided UUID.")
     @ResponseStatus(HttpStatus.OK)
     public void deleteTheEntity(@PathVariable("theEntityId") final UUID theEntityId) {
         theEntityService.deleteTheEntity(theEntityId);
