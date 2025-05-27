e433908464bfedca38a8ebd8199890475a9eccfe
jannik.lange <jannik.lange@muenchen.de>
2025-05-27 11:25:09 +0200

    :alien: generated first api-version

---
+++ refarch-frontend/src/api/refarch_backend-spec/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './PageMetadata';
+export * from './PagedModelTheEntityResponseDTO';
+export * from './TheEntityRequestDTO';
+export * from './TheEntityResponseDTO';
