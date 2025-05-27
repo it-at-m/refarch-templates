116ae3918c9484978cc829514b626466611658ed
jannik.lange <jannik.lange@muenchen.de>
2025-05-27 16:33:14 +0200

    :wrench: introduced properties class for app-information

---
+++ refarch-backend/src/main/java/de/muenchen/refarch/configuration/OpenAPIProperties.java
@@ -0,0 +1,21 @@
+package de.muenchen.refarch.configuration;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@ConfigurationProperties(prefix = "info.application")
+@Data
+@Validated
+public class OpenAPIProperties {
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private String version;
+}
