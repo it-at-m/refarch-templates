116ba1d39c29101cd4d02dcfe17a286187293397
langehm <38280183+langehm@users.noreply.github.com>
2025-07-07 15:20:32 +0200

    revised comments

--- refarch-backend/src/main/java/de/muenchen/refarch/configuration/SecurityConfiguration.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/configuration/SecurityConfiguration.java
@@ -52,7 +52,7 @@ public class SecurityConfiguration {
                         PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.GET, "/v3/api-docs.yaml"),
                         PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.GET, "/v3/api-docs/**"),
                         // allow access to swagger-ui
-                        PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.GET, "/swagger-ui/**"),
+                        PathPatternRequestMatcher.withDefaults().matcher("/swagger-ui/**"),
                         PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.GET, "/actuator/sbom"),
                         // allow access to SBOM application data
                         PathPatternRequestMatcher.withDefaults().matcher(HttpMethod.GET, "/actuator/sbom/application"),
