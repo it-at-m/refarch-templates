761b084d2903387c39553dee12bbe6ee2fe7bec5
devtobi <28538704+devtobi@users.noreply.github.com>
2025-10-09 09:41:24 +0200

    :wrench: Enable history mode based routing by default

--- refarch-frontend/src/plugins/router.ts
+++ refarch-frontend/src/plugins/router.ts
@@ -1,5 +1,5 @@
 // Composables
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import { ROUTES_GETSTARTED, ROUTES_HOME } from "@/constants";
 import GetStartedView from "@/views/GetStartedView.vue";
@@ -21,7 +21,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior() {
     return {
