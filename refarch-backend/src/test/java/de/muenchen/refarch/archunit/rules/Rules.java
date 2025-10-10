8cc94f3f18f8de95a232c7e5dd38ab55b3d16880
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-10-10 13:32:54 +0200

    coderabbit review eingearbeitet

--- refarch-backend/src/test/java/de/muenchen/refarch/archunit/rules/Rules.java
+++ refarch-backend/src/test/java/de/muenchen/refarch/archunit/rules/Rules.java
@@ -9,31 +9,43 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.params.ParameterizedTest;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Rules {
 
     public static final ArchRule RULE_TEST_NAMING_CONVENTION_GIVEN_THEN_MATCHED = methods()
-            .that().areAnnotatedWith(Test.class).or().areAnnotatedWith(ParameterizedTest.class)
+            .that().areAnnotatedWith(Test.class)
+            .or().areAnnotatedWith(ParameterizedTest.class)
+            .or().areAnnotatedWith(RepeatedTest.class)
+            .or()
+            .areAnnotatedWith(TestTemplate.class)
             .should().haveNameMatching("^given[A-Z][a-zA-Z]+_then[A-Z][a-zA-Z]+$");
 
     public static final ArchRule RULE_BEFORE_EACH_NAMING_CONVENTION_MATCHED = methods()
-            .that().areAnnotatedWith(BeforeEach.class).should().haveNameMatching("setUp")
+            .that().areAnnotatedWith(BeforeEach.class).should().haveNameMatching("^setUp$")
             .allowEmptyShould(true);
 
     public static final ArchRule RULE_AFTER_EACH_NAMING_CONVENTION_MATCHED = methods()
-            .that().areAnnotatedWith(AfterEach.class).should().haveNameMatching("tearDown")
+            .that().areAnnotatedWith(AfterEach.class).should().haveNameMatching("^tearDown$")
             .allowEmptyShould(true);
 
     public static final ArchRule RULE_TEST_METHODS_ARE_PACKAGE_PRIVATE_CONVENTION_MATCHED = methods()
-            .that().areAnnotatedWith(Test.class).or().areAnnotatedWith(ParameterizedTest.class).should()
+            .that().areAnnotatedWith(Test.class)
+            .or().areAnnotatedWith(ParameterizedTest.class)
+            .or().areAnnotatedWith(RepeatedTest.class).or()
+            .areAnnotatedWith(TestTemplate.class).should()
             .notHaveModifier(JavaModifier.PROTECTED)
             .andShould().notHaveModifier(JavaModifier.PRIVATE)
             .andShould().notHaveModifier(JavaModifier.PUBLIC);
 
     public static final ArchRule RULE_TESTCLASSES_END_WITH_TEST_CONVENTION_MATCHED = methods()
-            .that().areAnnotatedWith(Test.class).or().areAnnotatedWith(ParameterizedTest.class)
+            .that().areAnnotatedWith(Test.class)
+            .or().areAnnotatedWith(ParameterizedTest.class)
+            .or().areAnnotatedWith(RepeatedTest.class).or()
+            .areAnnotatedWith(TestTemplate.class)
             .should(haveTopEnclosingClassEndingWithTest);
 }
