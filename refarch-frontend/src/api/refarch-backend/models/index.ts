9b38c8a046220ae8b9a992e5d637c2a0212d0055
langehm <38280183+langehm@users.noreply.github.com>
2026-01-15 14:49:51 +0100

    regenerated openapi client with newer configuration

--- refarch-frontend/src/api/refarch-backend/models/index.ts
+++ refarch-frontend/src/api/refarch-backend/models/index.ts
@@ -1,5 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './Link';
 export * from './PageMetadata';
 export * from './PagedModelTheEntityResponseDTO';
 export * from './TheEntityRequestDTO';
