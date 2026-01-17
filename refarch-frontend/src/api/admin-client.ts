de5ee2ede1841a584c07e6b188230c7c8c7c63a4
ThomasAFink <thomas.fink@muenchen.de>
2026-01-17 18:29:30 +0100

    fix(CMS): remove unused error variable

--- refarch-frontend/src/api/admin-client.ts
+++ refarch-frontend/src/api/admin-client.ts
@@ -21,7 +21,7 @@ export function getAdminStatus(): Promise<AdminStatusResponse> {
         response,
         "Beim Laden des Admin-Status ist ein Fehler aufgetreten."
       );
-      return response.json().catch((error) => {
+      return response.json().catch(() => {
         throw new ApiError({
           message: "Beim Laden des Admin-Status ist ein Fehler aufgetreten.",
         });
