3ed22d71f0157849e311a70f825a15fa03bb0059
ThomasAFink <thomas.fink@muenchen.de>
2026-01-18 01:13:06 +0100

    refactor(CMS): remove redundant @ResponseStatus annotation
    - Remove @ResponseStatus(HttpStatus.OK) from AdminController.getAdminStatus()
    - Spring MVC returns 200 OK by default for successful responses
    - Remove unused HttpStatus import

--- refarch-backend/src/main/java/de/muenchen/refarch/admin/AdminController.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/admin/AdminController.java
@@ -2,11 +2,9 @@ package de.muenchen.refarch.admin;
 
 import de.muenchen.refarch.admin.dto.AdminStatusResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -25,7 +23,6 @@ public class AdminController {
      * @return JSON response with granted status
      */
     @GetMapping("/status")
-    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('writer')")
     public AdminStatusResponse getAdminStatus() {
         log.debug("Admin status endpoint accessed");
