50c2341fa22fb907459acf69004b37e007a898e6
Tobias Stadler <28538704+devtobi@users.noreply.github.com>
2026-02-13 08:47:53 +0100

    :wrench: Moved to file based routing (#1393)

--- refarch-frontend/src/constants.ts
+++ refarch-frontend/src/constants.ts
@@ -1,6 +1,3 @@
-export const ROUTES_HOME = "home";
-export const ROUTES_GETSTARTED = "getstarted";
-
 export const AD2IMAGE_URL = import.meta.env.VITE_AD2IMAGE_URL;
 export const APPSWITCHER_URL = import.meta.env.VITE_APPSWITCHER_URL;
 
