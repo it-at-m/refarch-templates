9198b130e67ca28257e5562487a748a2db1a4df2
langehm <38280183+langehm@users.noreply.github.com>
2026-02-20 15:41:50 +0100

    :white_check_mark: updated example test tow work with new snackbar

--- refarch-frontend/tests/unit/example.spec.ts
+++ refarch-frontend/tests/unit/example.spec.ts
@@ -1,28 +1,30 @@
 import { shallowMount } from "@vue/test-utils";
-import { createPinia } from "pinia";
-import { describe, expect, test } from "vitest";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
+import { createPinia, setActivePinia } from "pinia";
+import { beforeEach, describe, expect, test } from "vitest";
 
-import TheSnackbar from "@/components/TheSnackbar.vue";
-import i18n from "@/plugins/i18n";
+import TheSnackbarQueue from "@/components/TheSnackbarQueue.vue";
+import { useSnackbarStore } from "@/stores/snackbar";
 
-const pinia = createPinia();
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+describe("TheSnackbarQueue.vue", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
 
-describe("TheSnackbar.vue", () => {
-  test("renders props.message when passed", () => {
-    const message = "Hello_World";
-    const wrapper = shallowMount(TheSnackbar, {
+  test("passes store queue to v-snackbar-queue", async () => {
+    const pinia = createPinia();
+
+    const wrapper = shallowMount(TheSnackbarQueue, {
       global: {
-        plugins: [pinia, vuetify, i18n],
+        plugins: [pinia],
       },
-      props: { message: message },
     });
-    expect(wrapper.html()).toContain(message);
+
+    const store = useSnackbarStore();
+
+    store.add({ message: "Test_Message" });
+    await wrapper.vm.$nextTick();
+
+    const queueComponent = wrapper.findComponent({ name: "v-snackbar-queue" });
+    expect(queueComponent.props("modelValue")).toEqual(store.queue);
   });
 });
