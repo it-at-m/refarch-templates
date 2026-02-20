7e410d37430ebe793e174f7cc11577cf67cf5775
langehm <38280183+langehm@users.noreply.github.com>
2026-02-20 15:40:22 +0100

    Changed the store to queue messages

--- refarch-frontend/src/stores/snackbar.ts
+++ refarch-frontend/src/stores/snackbar.ts
@@ -1,29 +1,64 @@
+import {
+  mdiAlertCircleOutline,
+  mdiAlertOutline,
+  mdiCheckCircleOutline,
+  mdiInformationOutline,
+} from "@mdi/js";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-import { STATUS_INDICATORS } from "@/constants";
-
-export interface SnackbarState {
+export interface SnackbarMessage {
   message: string | undefined;
-  level: STATUS_INDICATORS;
-  show: boolean;
+  timeout?: number;
+  level?: Levels;
+  icon?: string;
 }
 
+export const enum Levels {
+  SUCCESS = "success",
+  INFO = "info",
+  WARNING = "warning",
+  ERROR = "error",
+}
+
+interface SnackbarInput {
+  text: string | undefined;
+  timeout: number;
+  color: Levels;
+  icon: string;
+}
+
+const DEFAULTS: Record<Levels, Pick<SnackbarInput, "timeout" | "icon">> = {
+  [Levels.INFO]: { timeout: 2500, icon: mdiInformationOutline },
+  [Levels.SUCCESS]: { timeout: 2500, icon: mdiCheckCircleOutline },
+  [Levels.WARNING]: { timeout: 3500, icon: mdiAlertOutline },
+  [Levels.ERROR]: { timeout: 4000, icon: mdiAlertCircleOutline },
+} as const;
+
+const normalizeSnackbar = (input: SnackbarMessage): SnackbarInput => {
+  const color = input.level ?? Levels.INFO;
+  const defaults = DEFAULTS[color];
+
+  return {
+    text: input.message,
+    color,
+    icon: input.icon ?? defaults.icon,
+    timeout: input.timeout ?? defaults.timeout,
+  };
+};
+
+/**
+ * Store for messages which should be displayed by the snackbar
+ */
 export const useSnackbarStore = defineStore("snackbar", () => {
-  const message = ref<string | undefined>(undefined);
-  const level = ref(STATUS_INDICATORS.INFO);
-  const show = ref(false);
-  function showMessage(messageI: {
-    message?: string;
-    level?: STATUS_INDICATORS;
-    show?: boolean;
-  }): void {
-    message.value = messageI.message;
-    level.value = messageI.level ? messageI.level : STATUS_INDICATORS.INFO;
-    show.value = true;
+  const queue = ref<SnackbarInput[]>([]);
+
+  /**
+   * Adds default values if necessary
+   */
+  function add(message: SnackbarMessage) {
+    queue.value.push(normalizeSnackbar(message));
   }
-  function updateShow(showI: boolean): void {
-    show.value = showI;
-  }
-  return { message, level, show, showMessage, updateShow };
+
+  return { queue, add };
 });
