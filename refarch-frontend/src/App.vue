09b9b044368da7a71bb2b525d74139ae2cd61c41
Tobias Stadler <28538704+devtobi@users.noreply.github.com>
2026-02-27 16:51:10 +0100

    :recycle: Refactored App component into dedicated components (#1416)

--- refarch-frontend/src/App.vue
+++ refarch-frontend/src/App.vue
@@ -1,69 +1,8 @@
 <template>
   <v-app>
     <the-snackbar-queue />
-    <v-app-bar color="primary">
-      <v-row align="center">
-        <v-col
-          cols="3"
-          class="d-flex align-center justify-start"
-        >
-          <v-app-bar-nav-icon @click.stop="toggleDrawer()" />
-          <router-link to="/">
-            <v-toolbar-title class="font-weight-bold">
-              <span class="text-white">{{ t("app.name.part1") }}</span>
-              <span class="text-secondary">{{ t("app.name.part2") }}</span>
-              <span class="text-white">{{ t("app.name.part3") }}</span>
-            </v-toolbar-title>
-          </router-link>
-        </v-col>
-        <v-col
-          cols="6"
-          class="d-flex align-center justify-center"
-        >
-          <v-text-field
-            id="searchField"
-            v-model="query"
-            flat
-            variant="solo-inverted"
-            hide-details
-            :label="t('app.search')"
-            clearable
-            :prepend-inner-icon="mdiMagnify"
-            theme="dark"
-            @keyup.enter="search"
-          />
-        </v-col>
-        <v-col
-          cols="3"
-          class="d-flex align-center justify-end"
-        >
-          <app-switcher
-            v-if="appswitcherBaseUrl"
-            :base-url="appswitcherBaseUrl"
-            :tags="['global']"
-            :icon="mdiApps"
-          />
-          <v-btn
-            variant="text"
-            icon
-          >
-            <ad2-image-avatar
-              v-if="userStore.getUser !== null"
-              :username="userStore.getUser.username"
-            />
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-app-bar>
-    <v-navigation-drawer v-model="drawer">
-      <v-list>
-        <v-list-item to="/getstarted">
-          <v-list-item-title>
-            {{ t("views.getStarted.navText") }}
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <the-app-bar @clicked-nav-icon="toggleNavigation" />
+    <the-navigation-drawer v-model="isNavigationShown" />
     <v-main>
       <v-container fluid>
         <router-view v-slot="{ Component }">
@@ -77,28 +16,18 @@
 </template>
 
 <script setup lang="ts">
-import { mdiApps, mdiMagnify } from "@mdi/js";
-import { AppSwitcher } from "@muenchen/appswitcher-vue";
 import { useToggle } from "@vueuse/core";
-import { onMounted, ref } from "vue";
-import { useI18n } from "vue-i18n";
+import { onMounted } from "vue";
 
 import { getUser } from "@/api/user-client";
-import Ad2ImageAvatar from "@/components/common/Ad2ImageAvatar.vue";
+import TheAppBar from "@/components/TheAppBar.vue";
+import TheNavigationDrawer from "@/components/TheNavigationDrawer.vue";
 import TheSnackbarQueue from "@/components/TheSnackbarQueue.vue";
-import { APPSWITCHER_URL } from "@/constants";
-import { useSnackbarStore } from "@/stores/snackbar";
 import { useUserStore } from "@/stores/user";
 import User, { UserLocalDevelopment } from "@/types/User";
 
-const { t } = useI18n();
-
-const query = ref<string>("");
-const appswitcherBaseUrl = APPSWITCHER_URL;
-
-const snackbarStore = useSnackbarStore();
 const userStore = useUserStore();
-const [drawer, toggleDrawer] = useToggle();
+const [isNavigationShown, toggleNavigation] = useToggle();
 
 onMounted(() => {
   loadUser();
@@ -118,16 +47,5 @@ function loadUser(): void {
         userStore.setUser(null);
       }
     });
-}
-
-/**
- * Navigates to the page with the search results and sends an event to trigger further searches.
- */
-async function search(): Promise<void> {
-  if (query.value !== "" && query.value !== null) {
-    snackbarStore.push({
-      text: "Sie haben nach " + query.value + " gesucht. ;)",
-    });
-  }
 }
 </script>
---
+++ refarch-frontend/src/components/TheNavigationDrawer.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-navigation-drawer
+    v-model="showDrawer"
+    temporary
+  >
+    <v-list>
+      <v-list-item to="/getstarted">
+        <v-list-item-title>
+          {{ t("views.getStarted.navText") }}
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const showDrawer = defineModel<boolean>({ required: true });
+
+const { t } = useI18n();
+</script>
