08d15dfeb03666b76bba3a6530b4462515fcbef5
langehm <38280183+langehm@users.noreply.github.com>
2026-02-27 15:10:48 +0100

    :white_check_mark: fixed test and used createTestingPinia

--- refarch-frontend/tests/unit/example.spec.ts
+++ refarch-frontend/tests/unit/example.spec.ts
@@ -1,29 +1,27 @@
+import { createTestingPinia } from "@pinia/testing";
 import { shallowMount } from "@vue/test-utils";
-import { createPinia, setActivePinia } from "pinia";
-import { beforeEach, describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 
 import TheSnackbarQueue from "@/components/TheSnackbarQueue.vue";
 import i18n from "@/plugins/i18n";
 import { useSnackbarStore } from "@/stores/snackbar";
 
 describe("TheSnackbarQueue.vue", () => {
-  beforeEach(() => {
-    setActivePinia(createPinia());
-  });
-
   test("passes store queue to v-snackbar-queue", async () => {
-    const pinia = createPinia();
-
     const wrapper = shallowMount(TheSnackbarQueue, {
       global: {
-        plugins: [pinia, i18n],
+        plugins: [
+          createTestingPinia({
+            createSpy: vi.fn,
+          }),
+          i18n,
+        ],
       },
     });
 
     const store = useSnackbarStore();
 
     store.push({ text: "Test_Message" });
-    await wrapper.vm.$nextTick();
 
     const queueComponent = wrapper.findComponent({ name: "v-snackbar-queue" });
     expect(queueComponent.props("modelValue")).toEqual(store.queue);
