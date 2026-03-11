673916ea07ebd92e942a1275ee3bf34be500ffda
DanielOber <145556033+DanielOber@users.noreply.github.com>
2026-03-11 19:42:11 +0100

    better naming for archunit tests

--- refarch-backend/src/test/java/de/muenchen/refarch/archunit/ArchUnitTest.java
+++ refarch-backend/src/test/java/de/muenchen/refarch/archunit/ArchUnitTest.java
@@ -25,12 +25,16 @@ public class ArchUnitTest {
 
     public static Stream<Arguments> allTestClassesRulesToVerify() {
         return Stream.of(
-                Arguments.of("RULE_TESTCLASSES_END_WITH_TEST_CONVENTION_MATCHED",
+                Arguments.of("Test classes must end with 'Test'",
                         Rules.RULE_TESTCLASSES_END_WITH_TEST_CONVENTION_MATCHED),
-                Arguments.of("TEST_NAMING_CONVENTION_RULE", Rules.RULE_TEST_NAMING_CONVENTION_GIVEN_THEN_MATCHED),
-                Arguments.of("RULE_BEFORE_EACH_NAMING_CONVENTION_MATCHED", Rules.RULE_BEFORE_EACH_NAMING_CONVENTION_MATCHED),
-                Arguments.of("RULE_AFTER_EACH_NAMING_CONVENTION_MATCHED", Rules.RULE_AFTER_EACH_NAMING_CONVENTION_MATCHED),
-                Arguments.of("TEST_METHODS_ARE_PACKAGE_PRIVATE_CONVENTION_MATCHED", Rules.RULE_TEST_METHODS_ARE_PACKAGE_PRIVATE_CONVENTION_MATCHED));
+                Arguments.of("Test methods must follow 'given_then' naming convention",
+                        Rules.RULE_TEST_NAMING_CONVENTION_GIVEN_THEN_MATCHED),
+                Arguments.of("BeforeEach methods must follow naming convention",
+                        Rules.RULE_BEFORE_EACH_NAMING_CONVENTION_MATCHED),
+                Arguments.of("AfterEach methods must follow naming convention",
+                        Rules.RULE_AFTER_EACH_NAMING_CONVENTION_MATCHED),
+                Arguments.of("Test methods should be package-private",
+                        Rules.RULE_TEST_METHODS_ARE_PACKAGE_PRIVATE_CONVENTION_MATCHED));
     }
 
     @ParameterizedTest(name = "{0}")
