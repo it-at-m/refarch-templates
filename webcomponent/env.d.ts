b9d45ce7c5b45f268b1cefd89ba3487dac3fd691
langehm <38280183+langehm@users.noreply.github.com>
2026-04-08 13:15:12 +0200

    added base path as env variable / type to dev and production

--- webcomponent/env.d.ts
+++ webcomponent/env.d.ts
@@ -5,3 +5,11 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_PATH: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
