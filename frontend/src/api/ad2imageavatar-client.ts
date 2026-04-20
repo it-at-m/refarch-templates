d299f8c717ecf2f0c5f602f341023d44f98a9278
devtobi <28538704+devtobi@users.noreply.github.com>
2026-04-20 10:59:29 +0200

    :recycle: Removed runtime check of env variable for now, will be handled in a later issue

--- frontend/src/api/ad2imageavatar-client.ts
+++ frontend/src/api/ad2imageavatar-client.ts
@@ -12,11 +12,6 @@ export function getAvatarHref(
   mode = "fallbackGeneric",
   size = "64"
 ): string {
-  if (!AD2IMAGE_URL) {
-    throw new Error(
-      "VITE_AD2IMAGE_URL was not configured. Please use the .env files."
-    );
-  }
   const url = new URL("/avatar", AD2IMAGE_URL);
   url.searchParams.append("uid", username);
   url.searchParams.append("m", mode);
