4758c1e56b28d217ea8794d3c7743b37c605f6f0
devtobi <28538704+devtobi@users.noreply.github.com>
2026-04-23 13:19:27 +0200

    :bug: Fixed example test

--- frontend/tests/unit/example.spec.ts
+++ frontend/tests/unit/example.spec.ts
@@ -5,25 +5,33 @@ import { describe, expect, test, vi } from "vitest";
 import TheSnackbarQueue from "@/components/TheSnackbarQueue.vue";
 import i18n from "@/plugins/i18n";
 import { useSnackbarStore } from "@/stores/snackbar";
+import { nextTick } from "vue";
 
 describe("TheSnackbarQueue.vue", () => {
   test("passes store queue to v-snackbar-queue", async () => {
+    // given
     const wrapper = shallowMount(TheSnackbarQueue, {
       global: {
         plugins: [
           createTestingPinia({
             createSpy: vi.fn,
+            stubActions: false
           }),
           i18n,
         ],
       },
     });
+    const text = "Test_Message";
 
+    // when
     const store = useSnackbarStore();
+    store.push({ text });
+    await nextTick();
 
-    store.push({ text: "Test_Message" });
-
+    // then
     const queueComponent = wrapper.findComponent({ name: "v-snackbar-queue" });
-    expect(queueComponent.props("modelValue")).toEqual(store.queue);
+    expect(queueComponent.props("modelValue")).toContainEqual(
+      expect.objectContaining({ text })
+    );
   });
 });
