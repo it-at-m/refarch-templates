75b7e44a0332883266b71a7f5a13f258bbd70a12
devtobi <28538704+devtobi@users.noreply.github.com>
2026-05-18 16:39:30 +0200

    :recycle: Refactored userInfo loading to router and store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,30 +16,11 @@
 </template>
 
 <script setup lang="ts">
-import type { UserInfo } from "@/types/UserInfo";
-
 import { useToggle } from "@vueuse/core";
-import { onMounted } from "vue";
 
-import { getUserInfo } from "@/api/userinfo-client";
 import TheAppBar from "@/components/TheAppBar.vue";
 import TheNavigationDrawer from "@/components/TheNavigationDrawer.vue";
 import TheSnackbarQueue from "@/components/TheSnackbarQueue.vue";
-import { useUserInfoStore } from "@/stores/userinfo";
 
-const userInfoStore = useUserInfoStore();
 const [isNavigationShown, toggleNavigation] = useToggle();
-
-onMounted(() => {
-  loadUserInfo();
-});
-
-/**
- * Loads UserInfo from the backend and sets it in the store.
- */
-function loadUserInfo(): void {
-  getUserInfo().then((userInfo: UserInfo) =>
-    userInfoStore.setUserInfo(userInfo)
-  );
-}
 </script>
