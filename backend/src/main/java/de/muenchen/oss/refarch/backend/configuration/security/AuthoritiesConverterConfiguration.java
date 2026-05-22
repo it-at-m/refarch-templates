d2828dc01730c7b664713b7005f7c118d4041bf7
devtobi <28538704+devtobi@users.noreply.github.com>
2026-05-22 16:19:22 +0200

    :rotating_light: Fixed lint

--- backend/src/main/java/de/muenchen/oss/refarch/backend/configuration/security/AuthoritiesConverterConfiguration.java
+++ backend/src/main/java/de/muenchen/oss/refarch/backend/configuration/security/AuthoritiesConverterConfiguration.java
@@ -1,13 +1,12 @@
 package de.muenchen.oss.refarch.backend.configuration.security;
 
+import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
-
-import java.time.Duration;
 
 /**
  * Configure how authorities are resolved.
