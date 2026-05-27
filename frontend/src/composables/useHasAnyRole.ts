1323fdc349591fd90a04dfd8afdd9c9bc79bf902
devtobi <28538704+devtobi@users.noreply.github.com>
2026-05-27 08:56:21 +0200

    :rotating_light: Applied suggestions

--- frontend/src/composables/useHasAnyRole.ts
+++ frontend/src/composables/useHasAnyRole.ts
@@ -10,13 +10,16 @@ export default function useHasAnyRole(roles: Role | Role[]) {
 }
 
 /**
- * Helper function: Only used in route guard, do not use directly
- * @param roles
- * @param currentRoles
+ * Helper function: Only use directly in route guard, use useHasAnyRole in components.
+ * @param requiredRoles - A single role or an array of roles to check against the user's roles.
+ * @param userRoles An array of roles that the user currently possesses.
+ * @return true if the user has at least one of the required roles, otherwise false.
  */
-export function hasAnyRole(roles: Role | Role[], currentRoles: Role[]) {
-  const requiredRoles = Array.isArray(roles) ? roles : [roles];
-  return requiredRoles.some((requiredRole) =>
-    currentRoles.includes(requiredRole)
+export function hasAnyRole(requiredRoles: Role | Role[], userRoles: Role[]) {
+  const requiredRolesArray = Array.isArray(requiredRoles)
+    ? requiredRoles
+    : [requiredRoles];
+  return requiredRolesArray.some((requiredRole) =>
+    userRoles.includes(requiredRole)
   );
 }
