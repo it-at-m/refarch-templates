825fa7b4a8e50ec73dfdf29fee723e4722a3b245
Tobias Stadler <28538704+devtobi@users.noreply.github.com>
2026-06-02 11:51:45 +0200

    :recycle: Refactored SimpleClientHttpRequestFactory to ClientHttpRequestFactoryBuilder (#1630)

--- backend/src/main/java/de/muenchen/oss/refarch/backend/configuration/security/AuthoritiesConverterConfiguration.java
+++ backend/src/main/java/de/muenchen/oss/refarch/backend/configuration/security/AuthoritiesConverterConfiguration.java
@@ -1,11 +1,13 @@
 package de.muenchen.oss.refarch.backend.configuration.security;
 
 import java.time.Duration;
+
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.HttpClientSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 /**
@@ -27,18 +29,18 @@ public class AuthoritiesConverterConfiguration {
     @Bean
     @Profile("keycloak-permissions")
     public KeycloakPermissionsAuthoritiesConverter keycloakPermissionsAuthoritiesConverter(
-            final SecurityProperties securityProperties, final RestClient.Builder restClientBuilder) {
+            final SecurityProperties securityProperties, final RestClient.Builder restClientBuilder,
+            final ClientHttpRequestFactoryBuilder<?> requestFactoryBuilder) {
         return new KeycloakPermissionsAuthoritiesConverter(
                 securityProperties,
                 restClientBuilder
-                        .requestFactory(clientHttpRequestFactory())
+                        .requestFactory(clientHttpRequestFactory(requestFactoryBuilder))
                         .build());
     }
 
-    private ClientHttpRequestFactory clientHttpRequestFactory() {
-        final SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(Duration.ofSeconds(KEYCLOAK_FETCH_TIMEOUT));
-        factory.setReadTimeout(Duration.ofSeconds(KEYCLOAK_FETCH_TIMEOUT));
-        return factory;
+    private ClientHttpRequestFactory clientHttpRequestFactory(final ClientHttpRequestFactoryBuilder<?> requestFactoryBuilder) {
+        return requestFactoryBuilder
+                .build(HttpClientSettings.defaults().withConnectTimeout(Duration.ofSeconds(KEYCLOAK_FETCH_TIMEOUT))
+                        .withReadTimeout(Duration.ofSeconds(KEYCLOAK_FETCH_TIMEOUT)));
     }
 }
