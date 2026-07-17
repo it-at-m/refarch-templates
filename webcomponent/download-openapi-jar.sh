9d4ee9a96dd5accbb05e1a5067532594c30c44f8
devtobi <28538704+devtobi@users.noreply.github.com>
2026-07-17 13:26:18 +0200

    :rotating_light: applied suggestions

--- webcomponent/download-openapi-jar.sh
+++ webcomponent/download-openapi-jar.sh
@@ -7,7 +7,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 echo "Starting download script ..."
 
 echo "----------------------------------------"
-echo "Project: $SCRIPT_DIR"
 
 # Reading version number of openapi-config file
 CFG="$SCRIPT_DIR/openapitools.json"
@@ -25,7 +24,7 @@ if [ -z "${VER:-}" ]; then
   exit 1
 fi
 
-echo " => Selected version in config-file: $VER"
+echo "Selected version in config-file: $VER"
 
 # Remote filename and location of jar
 REMOTE_JAR="openapi-generator-cli-$VER.jar"
@@ -37,7 +36,7 @@ mkdir -p "$DIR"
 
 # Check if jar file already exists
 if [ -f "$DIR/$LOCAL_JAR" ]; then
-  echo " => .jar file already exists: $DIR/$LOCAL_JAR, skipping download."
+  echo ".jar file already exists: $DIR/$LOCAL_JAR, skipping download."
   echo "----------------------------------------"
   echo "Done ..."
   exit 0
@@ -49,12 +48,12 @@ if ! command -v mvn > /dev/null 2>&1; then
   exit 1
 fi
 
-echo " => Downloading .jar file using Maven:"
-echo "    Target-file: $DIR/$LOCAL_JAR"
+echo "Downloading .jar file using Maven..."
 
-mvn dependency:copy "-Dartifact=org.openapitools:openapi-generator-cli:$VER:jar" "-DoutputDirectory=$DIR"
+mvn dependency:copy "-Dartifact=org.openapitools:openapi-generator-cli:$VER:jar" "-DoutputDirectory=$DIR" 1>/dev/null
 mv "$DIR/$REMOTE_JAR" "$DIR/$LOCAL_JAR"
 
-echo " => Download completed"
+echo "Download completed"
+echo "File downloaded to $DIR/$LOCAL_JAR"
 echo "----------------------------------------"
 echo "Done ..."
