b32497befabe2653846a91bbbb961f54755da90e
Tobias Stadler <28538704+devtobi@users.noreply.github.com>
2026-09-03 07:52:28 +0200

    :bug: Fixed wrong unauthorized handling in nested Vue routes (#1832)

--- webcomponent/src/api/appinfo-client.ts
+++ webcomponent/src/api/appinfo-client.ts
@@ -3,7 +3,7 @@ import type { AppInfo } from "@/types/AppInfo";
 import { defaultResponseHandler, getConfig } from "@/api/fetch-utils";
 
 export function getAppInfo(): Promise<AppInfo> {
-  return fetch("actuator/info", getConfig())
+  return fetch("/actuator/info", getConfig())
     .then((response) => {
       defaultResponseHandler(response);
       return response.json();
