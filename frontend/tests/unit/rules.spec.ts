827b138159026229e450ec3ff4dd642d6179799f
devtobi <28538704+devtobi@users.noreply.github.com>
2026-09-03 15:38:15 +0200

    :white_check_mark: Added unit tests for the implemented custom rules

---
+++ frontend/tests/unit/rules.spec.ts
@@ -0,0 +1,96 @@
+import { assert, describe, test } from "vitest";
+
+import { maxRule, minRule, uniqueRule } from "../../src/plugins/rules";
+
+describe("min rule tests", () => {
+  test("Throws error when too low", () => {
+    // given
+    const num = 5;
+    const validationRule = minRule(10);
+
+    // when
+    const result = validationRule(num);
+
+    // then
+    assert.isString(result);
+  });
+
+  test("Throws no error when high enough", () => {
+    // given
+    const num = 10;
+    const validationRule = minRule(10);
+
+    // when
+    const result = validationRule(num);
+
+    // then
+    assert.isTrue(result);
+  });
+});
+
+describe("max rule tests", () => {
+  test("Throws error when too high", () => {
+    // given
+    const num = 15;
+    const validationRule = maxRule(10);
+
+    // when
+    const result = validationRule(num);
+
+    // then
+    assert.isString(result);
+  });
+
+  test("Throws no error when low enough", () => {
+    // given
+    const num = 10;
+    const validationRule = maxRule(10);
+
+    // when
+    const result = validationRule(num);
+
+    // then
+    assert.isTrue(result);
+  });
+});
+
+describe("unique rule tests", () => {
+  test("Throws an error when value already exists and no initialValue", () => {
+    // given
+    const newValue = 10;
+    const existingValues = [5, 8, 10];
+    const validationRule = uniqueRule(existingValues, undefined);
+
+    // when
+    const result = validationRule(newValue);
+
+    // then
+    assert.isString(result);
+  });
+
+  test("Throws no error when value does not exists", () => {
+    // given
+    const newValue = 10;
+    const existingValues = [5, 8];
+    const validationRule = uniqueRule(existingValues, undefined);
+
+    // when
+    const result = validationRule(newValue);
+
+    // then
+    assert.isTrue(result);
+  });
+
+  test("Throws no error when value still exists and is initialValue", () => {
+    // given
+    const newValue = 10;
+    const existingValues = [5, 8, 10];
+    const validationRule = uniqueRule(existingValues, newValue);
+
+    // when
+    const result = validationRule(newValue);
+
+    // then
+    assert.isTrue(result);
+  });
+});
