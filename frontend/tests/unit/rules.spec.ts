9771d1f6fa15fe3f03d92c461937c2ac0e29e87b
devtobi <28538704+devtobi@users.noreply.github.com>
2026-09-14 12:14:58 +0200

    :recycle: Refactor rules tests to parameterized

--- frontend/tests/unit/rules.spec.ts
+++ frontend/tests/unit/rules.spec.ts
@@ -3,131 +3,88 @@ import { assert, describe, test } from "vitest";
 import { maxRule, minRule, uniqueRule } from "../../src/plugins/rules";
 
 describe("min rule tests", () => {
-  test("Throws error when too low", () => {
+  test.concurrent.for([
+    { num: 5, expected: "error" },
+    { num: 10, expected: "valid" },
+    { num: undefined, expected: "valid" },
+  ])("validates $num as $expected", ({ num, expected }) => {
     // given
-    const num = 5;
     const validationRule = minRule(10);
 
     // when
     const result = validationRule(num);
 
     // then
-    assert.isString(result);
-  });
-
-  test("Throws no error when high enough", () => {
-    // given
-    const num = 10;
-    const validationRule = minRule(10);
-
-    // when
-    const result = validationRule(num);
-
-    // then
-    assert.isTrue(result);
-  });
-
-  test("Throws no error when undefined", () => {
-    // given
-    const num = undefined;
-    const validationRule = minRule(10);
-
-    // when
-    const result = validationRule(num);
-
-    // then
-    assert.isTrue(result);
+    if (expected === "error") {
+      assert.isString(result);
+    } else {
+      assert.isTrue(result);
+    }
   });
 });
 
 describe("max rule tests", () => {
-  test("Throws error when too high", () => {
+  test.concurrent.for([
+    { num: 15, expected: "error" },
+    { num: 10, expected: "valid" },
+    { num: undefined, expected: "valid" },
+  ])("validates $num as $expected", ({ num, expected }) => {
     // given
-    const num = 15;
     const validationRule = maxRule(10);
 
     // when
     const result = validationRule(num);
 
     // then
-    assert.isString(result);
-  });
-
-  test("Throws no error when low enough", () => {
-    // given
-    const num = 10;
-    const validationRule = maxRule(10);
-
-    // when
-    const result = validationRule(num);
-
-    // then
-    assert.isTrue(result);
-  });
-
-  test("Throws no error when undefined", () => {
-    // given
-    const num = undefined;
-    const validationRule = maxRule(10);
-
-    // when
-    const result = validationRule(num);
-
-    // then
-    assert.isTrue(result);
+    if (expected === "error") {
+      assert.isString(result);
+    } else {
+      assert.isTrue(result);
+    }
   });
 });
 
 describe("unique rule tests", () => {
-  test("Throws an error when value already exists and no initialValue", () => {
-    // given
-    const newValue = 10;
-    const existingValues = [5, 8, 10];
-    const validationRule = uniqueRule(existingValues, undefined);
+  test.concurrent.for([
+    {
+      newValue: 10,
+      existingValues: [5, 8, 10],
+      initialValue: undefined,
+      expected: "error",
+    },
+    {
+      newValue: 10,
+      existingValues: [5, 8],
+      initialValue: undefined,
+      expected: "valid",
+    },
+    {
+      newValue: 10,
+      existingValues: [5, 8, 10],
+      initialValue: 10,
+      expected: "valid",
+    },
+    {
+      newValue: undefined,
+      existingValues: [5, 8, 10],
+      initialValue: undefined,
+      expected: "valid",
+    },
+  ])(
+    "validates $newValue as $expected",
+    ({ newValue, existingValues, initialValue, expected }) => {
+      // given
+      const validationRule = uniqueRule(existingValues, initialValue);
 
-    // when
-    const result = validationRule(newValue);
+      // when
+      const result = validationRule(newValue);
 
-    // then
-    assert.isString(result);
-  });
-
-  test("Throws no error when value does not exists", () => {
-    // given
-    const newValue = 10;
-    const existingValues = [5, 8];
-    const validationRule = uniqueRule(existingValues, undefined);
-
-    // when
-    const result = validationRule(newValue);
-
-    // then
-    assert.isTrue(result);
-  });
-
-  test("Throws no error when value still exists and is initialValue", () => {
-    // given
-    const newValue = 10;
-    const existingValues = [5, 8, 10];
-    const validationRule = uniqueRule(existingValues, newValue);
-
-    // when
-    const result = validationRule(newValue);
-
-    // then
-    assert.isTrue(result);
-  });
-
-  test("Throws no error when undefined", () => {
-    // given
-    const newValue = undefined;
-    const existingValues = [5, 8, 10];
-    const validationRule = uniqueRule(existingValues, newValue);
-
-    // when
-    const result = validationRule(newValue);
-
-    // then
-    assert.isTrue(result);
-  });
+      // then
+      if (expected === "error") {
+        assert.isString(result);
+      } else {
+        assert.isTrue(result);
+      }
+    }
+  );
 });
